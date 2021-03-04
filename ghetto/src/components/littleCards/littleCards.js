5ccddcb52926736ec5ecea031b3df44fa5b45d17
Arthur <solovyev.arthur@gmail.com>
2021-03-04 23:57:43 +0300

    fixed AJAX function

--- ghetto/src/components/littleCards/littleCards.js
+++ ghetto/src/components/littleCards/littleCards.js
@@ -7,32 +7,48 @@ import OverwatchService from '../overwatchService/overwatchService';
 export default class LittleCards extends React.Component {
   
 
-    state: {
+  state = {
         name: 'Pending',
         lvl: 'Pending',
     }
 
-    getResourse = async () => {
-      let x;
+  getPlayerStats = ( nick, id ) => {
       const request = new OverwatchService;
-      request.getResourse('frst', '2873');
+      request.getPlayerStats( nick, id );
     }
 
-    componentDidMount() {
-      this.getResourse();
-    }
-
+  updateData = () => {
+    this.getPlayerStats('frst', '2873').then( data =>
+      this.setState((data) => ({
+        name: data.name,
+        lvl: data.lvl,
+      })
+    ));
+  }
   
   render() {
+
     return (
         <div>
           <div className="p-3 my-2 rounded">
             <Toast>
               <ToastHeader>
-                Name is
+                Name is {this.state.name}
               </ToastHeader>
               <ToastBody>
-                Your level is
+                Your level is {this.state.lvl}
+              </ToastBody>
+              <ToastBody>
+                Your level is {this.state.lvl}
+              </ToastBody>
+              <ToastBody>
+                Your level is {this.state.lvl}
+              </ToastBody>
+              <ToastBody>
+                Your level is {this.state.lvl}
+              </ToastBody>
+              <ToastBody>
+                Your level is {this.state.lvl}
               </ToastBody>
             </Toast>
           </div>
--- ghetto/src/components/overwatchService/overwatchService.js
+++ ghetto/src/components/overwatchService/overwatchService.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class OverwatchService extends React.Component {
 
-    getResourse = async (uName, battleTag) => {
+    getPlayerStats = async (uName, battleTag) => {
         const request =  await fetch('https://owapi.io/stats/pc/us/' + uName + '-' + battleTag)
          .then(item => item.json()).then(item => console.log(item));
      }
