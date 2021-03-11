f700b96049afe975def6444aa7989274a6b1fdd8
Arthur <solovyev.arthur@gmail.com>
2021-03-11 23:55:06 +0300

    upd: fixes

--- ghetto/src/components/littleCards/littleCards.js
+++ ghetto/src/components/littleCards/littleCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Toast, ToastBody, ToastHeader } from 'reactstrap';
 import Spinner from '../spinner/spinner';
-
+import Api from '../api/api';
 
 export default class LittleCards extends React.Component {
   
@@ -9,36 +9,35 @@ export default class LittleCards extends React.Component {
     super(props);
     this.state = {
       statistics: {
-        username: 'pending...',
-        level: 'pending...',
-        wins: 'pending...',
-        loses: 'pending...',
-        bestHero: 'pending...',
-        dealedDmg: 'pending...',
-        healCount: 'pending...',
+        username: 'Загрузка...',
+        level: 'Загрузка...',
+        wins: 'Загрузка...',
+        loses: 'Загрузка...',
+        bestHero: 'Загрузка...',
+        dealedDmg: 'Загрузка...',
+        healCount: 'Загрузка...',
+        heroImg: 'Загрузка...',
       },
     }
   }
 
-  getStats = () => {
-    fetch('https://owapi.io/stats/pc/us/frst-2873')
-    .then(item => item.json())
-    .then(item => {
-      this.setState({ 
-        statistics: {
-          username: item.username,
-          level: item.level,
-          wins: item.stats.game.competitive[2].value + item.stats.game.quickplay[2].value,
-          loses: item.stats.game.competitive[0].value + item.stats.game.quickplay[0].value,
-          bestHero: item.stats.top_heroes.quickplay.games_won[0].hero,
-          dealedDmg: item.stats.combat.quickplay[0].value,
-          healCount: item.stats.assists.quickplay[1].value,
-        }
-      });
-      console.log(item);
+  getStats = async () => {
+    let data = await Api('frst', '2873');
+    console.log(data);
+    
+    this.setState({ 
+      statistics: {
+        username: data.username,
+        level: data.level,
+        wins: data.stats.game.competitive[2].value + data.stats.game.quickplay[2].value,
+        loses: data.stats.game.competitive[0].value + data.stats.game.quickplay[0].value,
+        bestHero: data.stats.top_heroes.quickplay.games_won[0].hero,
+        dealedDmg: data.stats.combat.quickplay[0].value,
+        healCount: data.stats.assists.quickplay[1].value,
+        heroImg: data.stats.top_heroes.quickplay.games_won[0].img,
+      }
     });
   }
-  
 
   componentDidMount() {
     this.getStats();
@@ -50,17 +49,18 @@ export default class LittleCards extends React.Component {
 
   render() {
 
-    const {username, level, wins, loses, bestHero, dealedDmg, healCount} = this.state.statistics;
+    const {username, level, wins, loses, bestHero, dealedDmg, healCount, heroImg} = this.state.statistics;
 
     const toaster = () => {
-      if (this.state.statistics.username === 'pending...') {
+      if (this.state.statistics.username === 'Загрузка...') {
         return(
           <Spinner />
         )
       } else {
         return(
-          <>
-          <ToastBody>
+          <div>
+              <div className='heroImg'><img src={heroImg} alt='pending'></img></div>
+              <ToastBody>
                 Уровень: <span><b>{level}</b></span>
               </ToastBody>
               <ToastBody>
@@ -75,7 +75,7 @@ export default class LittleCards extends React.Component {
               <ToastBody>
                 Всего восстановлено здоровья: <span><b>{healCount}</b></span>
               </ToastBody>
-          </>
+          </div>
         )
       }
     }
